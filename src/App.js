fa7b3e7383e251eac76cfe9370ebc691050158ab
rishabh5197 <68542463+rishabh5197@users.noreply.github.com>
2021-12-20 23:24:46 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <h1>ProStars</h1>
+      </header>
+      <main>
+        <div>
+          <Home />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
